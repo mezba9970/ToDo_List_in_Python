7d2bb3a396e5288f359cf4a26f018d18afd23397
mezba9970 <mezba2014@gmail.com>
2020-07-26 23:53:02 +0600

    add api function

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'tasks',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
